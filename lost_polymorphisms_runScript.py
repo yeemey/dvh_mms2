bb3fc1fd247f77bd11150cc7d9b51b844dca40ee
ymseah <yeemey@users.noreply.github.com>
2017-04-23 17:42:32 -0700

    DEV: refactor as methods, add script utilizing new methods.

---
+++ lost_polymorphisms_runScript.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 23 14:28:40 2017
+
+@author: ymseah
+"""
+from lost_polymorphisms import ComparePolymorphisms
+import glob
+
+input_directory = '/opt/data/wkim-rsrch/breseq_results/'
+output_directory = '/home/NETID/ymseah/Projects/lost_polymorphisms_in_breseq/results/'
+generations = [0, 100, 300, 500, 780, 1000]
+
+compare_files = glob.glob(input_directory + 'compare/[H|U]*.html')
+print('All gdtools COMPARE html files: ')
+print(compare_files)
+
+cp = ComparePolymorphisms()
+
+ancestor_df = cp.annotated_gd_to_df(input_directory + 'sic_Ancestor_breseq/output/0.gd', 0)
+
+for file in compare_files:
+    print(file)
+    evolution_line = str(file)[-6:-9]
+    print('Evolution line: ' + evolution_line)
+    bs_from_html = cp.parse_compare_html(file)
+    mutation_freqs_dict = cp.get_generation_frequencies(bs_from_html)
+    suspect_freqs_dict = cp.get_suspect_frequencies(mutation_freqs_dict)
+    annotated_gd_files = glob.glob(input_directory + 'sic_' + evolution_line + '*/output/*.gd')
+    print(annotated_gd_files)
+    all_dataframes = [ancestor_df]
+    for genome_diff in annotated_gd_files:
+        print(genome_diff)
+        generation = int(genome_diff[-4:-9].split('-')[1])
+        print(generation)
+        dataframe = cp.annotated_gd_to_df(genome_diff, generation)
+        all_dataframes.append(dataframe)
+    print(all_dataframes)
+    evolution_line_dataframe = cp.summary_df(evolution_line, all_dataframes, output_directory)
+    new_suspect_freqs_dict = cp.get_reject_reasons(evolution_line_dataframe, suspect_freqs_dict)
+    cp.write_frequency_dicts_to_file(suspect_freqs_dict, evolution_line + '_suspect')
+    cp.write_frequency_dicts_to_file(new_suspect_freqs_dict, evolution_line + '_suspect_reasons')     
