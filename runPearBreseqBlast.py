c62ad49dd5042ff041219465f48621995dce5acb
ymseah <yeemey@users.noreply.github.com>
2017-06-19 14:12:25 -0700

    added directory and sample variables

--- runPearBreseqBlast.py
+++ runPearBreseqBlast.py
@@ -13,6 +13,12 @@ from blastUnmapped import run_software, parse_results
 import re, os, subprocess
 
 run_object = run_software()
+input_dir = '/home/NETID/ymseah/Projects/ngs_preprocess/20170619/results/'
+forward_read = 'sic_UA3_S2_L001_R1_001.fastq'
+reverse_read = 'sic_UA3_S2_L001_R2_001.fastq'
+sample_name = 'sic_UA3_S2_L001'
+output_dir = '/home/NETID/ymseah/Projects/ngs_preprocess/20170619/results/'
+batch_reads_dir = '/opt/data/wkim-data/FilteredFastQC/'
 #print(run_object.get_all_pear_samples('/Users/ymseah/scriptingProjects/python/test/'))
 #run_object.batch_run_pear('/opt/data/wkim-data/FilteredFastQC/'', 
 #                          'home/NETID/ymseah/Projects/Low_Mapping_in_breseq/results/20161201/')
@@ -21,4 +27,7 @@ run_object = run_software()
 #run_object.run_breseq('/home/NETID/ymseah/Projects/Low_Mapping_in_breseq/results/20161202/',
 #                      'sic_HE3.118','/home/NETID/ymseah/Projects/Low_Mapping_in_breseq/results/breseq_results/')
 #run_object.run_gdtools_compare('sic_HE3-15', 'sic_HE3.45', 'sic_HE3-76', 'sic_HE3-118')
-run_object.run_blastn_remote('sic_HA3.45')
+#run_object.run_blastn_remote('sic_HA3.45')
+run_object.run_pear(input_dir + forward_read, input_dir + reverse_read, output_dir, sample_name)
+run_object.run_breseq(input_dir, sample_name, output_dir)
+
