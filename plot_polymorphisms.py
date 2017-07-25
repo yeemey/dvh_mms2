d7d544e6b4efc508fd12d8858b9cc70b279fdd0b
ymseah <yeemey@users.noreply.github.com>
2017-07-25 14:45:53 -0700

    DEV: modified to test get_all_gd() and gd_frequencies_to_df()

--- plot_polymorphisms.py
+++ plot_polymorphisms.py
@@ -11,14 +11,15 @@ import glob
 
 input_directory = '/opt/data/wkim-rsrch/breseq_results/'
 output_directory = '/home/NETID/ymseah/Projects/plot_polymorphisms/results/'
-generations = [0, 100, 300, 500, 780, 1000]
+ancestor_gd_path = input_directory + 'sic_Ancestor_breseq/output/0.gd'
 
+'''
+# frequencies from HTML output
 compare_files = glob.glob(input_directory + 'compare/[H|U]*.html')
 print('All gdtools COMPARE html files: ')
 print(compare_files)
 
 cp = ComparePolymorphisms()
-ancestor_df = cp.annotated_gd_to_df(input_directory + 'sic_Ancestor_breseq/output/0.gd', 0)
 for file in compare_files:
     print(file)
     evolution_line = file[-8:-5]
@@ -27,4 +28,9 @@ for file in compare_files:
     mutation_freqs_dict = cp.get_html_generation_frequencies(bs_from_html)
     output_csv = output_directory + evolution_line + '_html_freqs.csv'
     html_freqs_df = cp.html_frequencies_to_df(mutation_freqs_dict, save_csv=True, csv_filename=output_csv)
-    
+'''
+
+# frequencies from GD output
+cp_gd = ComparePolymorphisms()
+HA3_df = cp_gd.get_all_gd('HA3', input_directory, ancestor_gd_path)
+HA3_frequencies = cp_gd.gd_frequencies_to_df(HA3_df, save_csv=True, csv_filename='HA3_gd_freqs.csv')
