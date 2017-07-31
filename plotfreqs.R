c4ee0e801fdfb8d3281ab7e04ecc0cd4e065a5c6
ymseah <yeemey@users.noreply.github.com>
2017-07-31 14:24:29 -0700

    DEV: plotting function in R

---
+++ plotfreqs.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+csv_to_frequency_plot <- function(filepath) {
+  freqs <- read.csv(filepath)
+  polymorphisms <- freqs %>% 
+    unite(polymorphism, position, entry_type, mutation_detail, sep = ' ') %>%
+    select(genome_id, polymorphism, generation, polymorphism_frequency) %>% 
+    complete(nesting(polymorphism, genome_id), generation, fill = list(polymorphism_frequency = 0))
+  
+  ggplot(data = polymorphisms, mapping = aes(x = generation, y = polymorphism_frequency)) + 
+    geom_line(aes(group = polymorphism)) + 
+    facet_wrap(~ genome_id, ncol = 1)
+  ggsave(paste(filepath, 'plot.png', sep = '_'), last_plot())
+}
