95a960db4b18f36a41a9d7e84ebcf6af2274245c
ymseah <yeemey@users.noreply.github.com>
2017-08-11 12:51:22 -0700

    ENH: write new R df to csv

--- scripts/plotfreqs.R
+++ scripts/plotfreqs.R
@@ -6,7 +6,7 @@ csv_to_frequency_plot <- function(filepath) {
     unite(polymorphism, position, entry_type, mutation_detail, sep = ' ') %>%
     select(genome_id, polymorphism, generation, polymorphism_frequency) %>% 
     complete(nesting(polymorphism, genome_id), generation, fill = list(polymorphism_frequency = 0))
-  
+  write.csv(polymorphisms, file = paste(filepath, 'EDIT.csv', sep = '_'), row.names = FALSE, quote = FALSE)
   ggplot(data = polymorphisms, mapping = aes(x = generation, y = polymorphism_frequency)) + 
     geom_line(aes(group = polymorphism)) + 
     facet_wrap(~ genome_id, ncol = 1)
