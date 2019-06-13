e7f6fb6eb3ae7d107cbd5c13e91a95825f12cbd8
ymseah <yeemey@users.noreply.github.com>
2019-06-12 23:13:45 -0700

    Output mutation, evidence dataframes as csv

---
+++ scripts/filter_mutations/01_gd_to_dataframe.py
@@ -0,0 +1,12 @@
+from sys import argv
+import numpy as np
+import pandas as pd
+import tidy_breseq as tb
+
+script, line, input_dir, sample, generation = argv
+
+dfmut, dfev = tb.subset_gd_to_df(input_dir + '/output/evidence/annotated.gd', sample, line, generation, cov=True)
+complete_df = tb.combine_mutations_and_evidence(dfmut, dfev)
+complete_df.to_csv(sample+'_mutev.csv', index=False)
+dfmut.to_csv(sample+'_mutations.csv', index=False)
+dfev.to_csv(sample+'_evidence.csv', index=False)
